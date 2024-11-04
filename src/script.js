c4fcb68763e0e9125c09d2d294ba1b985d7d423a
Otávio Campezzi <90792115+OCampezzi@users.noreply.github.com>
2024-11-04 20:55:45 -0300

    wip

--- src/script.js
+++ src/script.js
@@ -1,11 +1,19 @@
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array;
+}
+
 function consult() {
   document.getElementById("map").classList.remove("d-none");
 
   document.getElementById("city").innerHTML = "";
   document.getElementById("courses").innerHTML = "";
 
-  let mesoregion = document.getElementById("mesorregiao").value;
-  console.log(mesoregion);
+  let mesoregion = document.getElementById("mesorregiao").value.trim();
 
   Promise.all([
     fetch("../data/dados_imp.json").then((response) => response.json()),
@@ -67,39 +75,82 @@ function consult() {
 
       document.getElementById("city").innerHTML = bestCity.cityName;
 
+      let jobsSum =
+        parseFloat(bestCity.cityData2["agro_jobs"]) +
+        parseFloat(bestCity.cityData2["industry_jobs"]) +
+        parseFloat(bestCity.cityData2["service_jobs"]);
+
       let sectors = [
         {
           name: "agricultura",
-          participation: parseFloat(bestCity.cityData2["agro_jobs"]) || 0,
+          participation:
+            parseFloat(bestCity.cityData2["agro_jobs"]) / jobsSum || 0,
         },
         {
           name: "industria",
-          participation: parseFloat(bestCity.cityData2["industry_jobs"]) || 0,
+          participation:
+            parseFloat(bestCity.cityData2["industry_jobs"]) / jobsSum || 0,
         },
         {
           name: "servicos",
-          participation: parseFloat(bestCity.cityData2["service_jobs"]) || 0,
+          participation:
+            parseFloat(bestCity.cityData2["service_jobs"]) / jobsSum || 0,
         },
       ];
 
-      sectors.sort((a, b) => b.participation - a.participation);
+      let sectorToCourses = cursos_por_area["cursos_por_area"];
 
-      let suggestedCourses = [];
+      let courseScores = {};
+
+      let maxCourses = 10;
+      let auxMaxCourses = 10;
+      let count = 0;
 
       sectors.forEach((sector) => {
-        let areaKey = sector.name;
-        if (areaKey && cursos_por_area["cursos_por_area"][areaKey]) {
-          suggestedCourses = suggestedCourses.concat(
-            cursos_por_area["cursos_por_area"][areaKey]
-          );
+        let sectorCourses = shuffle(sectorToCourses[sector.name]);
+
+        if (sectorCourses) {
+          let numCourses = Math.round(sector.participation * maxCourses);
+
+          ++count;
+
+          if (count == 3 && auxMaxCourses > 0) {
+            numCourses = auxMaxCourses;
+          }
+
+          numCourses = Math.min(numCourses, sectorCourses.length);
+
+          auxMaxCourses -= numCourses;
+
+          for (let i = 0; i < numCourses; i++) {
+            const course = sectorCourses[i];
+            if (courseScores[course]) {
+              courseScores[course] += sector.participation;
+            } else {
+              courseScores[course] = sector.participation;
+            }
+          }
         }
       });
 
-      suggestedCourses = [...new Set(suggestedCourses)].slice(0, 10);
+      let courseScoresArray = Object.keys(courseScores).map((course) => {
+        return {
+          course: course,
+          score: courseScores[course],
+        };
+      });
 
-      suggestedCourses.forEach((course) => {
+      courseScoresArray.sort((a, b) => b.score - a.score);
+
+      courseScoresArray = courseScoresArray.slice(0, maxCourses);
+
+      courseScoresArray.sort((a, b) => b.score - a.score);
+
+      let suggestedCourses = courseScoresArray.slice(0, 10);
+
+      suggestedCourses.forEach((courseObj) => {
         document.getElementById("courses").innerHTML +=
-          "<li>" + course + "</li>";
+          "<li>" + courseObj.course + "</li>";
       });
 
       [...document.querySelectorAll(".city")].forEach((element) => {
@@ -107,7 +158,6 @@ function consult() {
       });
 
       let code = bestCity.cityData2["ibge_code"];
-      console.log(code);
 
       document.getElementById("mun_" + code).classList.remove("city-off");
     })
