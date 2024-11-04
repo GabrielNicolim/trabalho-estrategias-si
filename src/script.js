b690274267a97564e43721ed58b17d545f32830b
Gabriel Gomes Nicolim <gabriel.gomes.nicolim@gmail.com>
2024-11-04 19:23:46 -0300

    wip

--- src/script.js
+++ src/script.js
@@ -3,11 +3,23 @@ function consult()
     window.document.getElementById('map').classList.remove('d-none');
 
     window.document.getElementById('city').innerHTML = '';
+
     window.document.getElementById('courses').innerHTML = '';
 
     let courses = [
         1, 2, 3, 4, 5, 6, 7, 8
     ];
+
+
+    [...document.querySelectorAll(".city")].map(
+        (element, index, array) => {
+            element.classList.add('city-off');
+        }
+    );
+
+    let code = '3555406';
+
+    window.document.getElementById('mun_' + code).classList.remove('city-off');
 
     window.document.getElementById('city').innerHTML = 'Cidade!!!';
 
